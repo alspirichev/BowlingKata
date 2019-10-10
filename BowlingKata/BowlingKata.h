d12cdd590a6a5d912a5153a95a0fa05599f6cca7
Alexander Spirichev <alspirichev@gmail.com>
2019-10-10 16:11:09 +0300

    Initial Commit

---
+++ BowlingKata/BowlingKata.h
@@ -0,0 +1,19 @@
+//
+//  BowlingKata.h
+//  BowlingKata
+//
+//  Created by Alexander Spirichev on 10/10/2019.
+//  Copyright © 2019 Alexander Spirichev. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BowlingKata.
+FOUNDATION_EXPORT double BowlingKataVersionNumber;
+
+//! Project version string for BowlingKata.
+FOUNDATION_EXPORT const unsigned char BowlingKataVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BowlingKata/PublicHeader.h>
+
+
